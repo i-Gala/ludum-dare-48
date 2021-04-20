a0250d2a1cfc2fdb2cadb1b6962be7bd1ea8b318
Gala <gala.garcia@outlook.es>
2021-04-20 21:48:31 +0200

    Empty Project (4.26.2)

---
+++ Source/ld48/ld48GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ld48GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LD48_API Ald48GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
